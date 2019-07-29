7a2e36d22e9bf6de9a13c332df56a3c704ae3233
Mr1Ng <37356837+Mr1Ng@users.noreply.github.com>
2019-07-29 16:55:07 +0800

    Add files via upload

---
+++ JavaOOP-master/src/main/TestStudent.java
@@ -0,0 +1,10 @@
+package main;
+
+import model.Student;
+
+public class TestStudent {
+    public static void main(String[] args) {
+        Student s = new Student("小明",18,2019071515,"男");
+        System.out.println(s);
+    }
+}
